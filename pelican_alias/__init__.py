75ff37f8c9d7d03b9e70778cce3bdd400f10a9e8
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2017-02-04 14:30:51 -0500

    Correct imports

--- pelican_alias/__init__.py
+++ pelican_alias/__init__.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 
 import os.path
 import logging
-from urlparse import urlparse
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 from pelican import signals
 
